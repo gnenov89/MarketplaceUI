9f77f78b07206ef846437a812ce4d08fdf3a7c03
Georgi <georgi.nenov89@gmail.com>
2022-02-24 16:00:21 -0500

    routes added

---
+++ marketplace-ui/src/pages/Explore.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Explore() {
+  return (
+    <div>
+      Some jsx goes here
+    </div>
+  )
+}
+
+export default Explore
---
+++ marketplace-ui/src/pages/SignIN.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIN() {
+  return (
+    <div>
+      Sign IN
+    </div>
+  )
+}
+
+export default SignIN
